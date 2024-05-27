b8489b80312fccf7a9081864b4b89c2846c0299c
Martiwj <Martin.wich.jorg@gmail.com>
2024-05-28 00:52:23 +0200

    small fix

--- eksamen.py
+++ eksamen.py
@@ -1,22 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
-from datetime import datetime
 import xlsxwriter
 
 
 
-def fetch_exam_info(url, exam_info, emne):
-    # Send en GET-forespørsel til URL-en
-    språkkode = 'no'  # for norsk, 'en' for engelsk, osv.
-
-    # Definer forespørselshodet med språkinnstillingen
-    headers = {'Accept-Language': språkkode}
+def fetch_exam_info(url, exam_info, topic):
 
     # Send en GET-forespørsel til URL-en med det angitte språket
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     
-    # Sjekk om forespørselen var vellykket
+    # Se om forespørselen var vellykket
     if response.status_code != 200:
         print("Feil ved henting av nettsiden.")
         return
@@ -24,31 +18,22 @@ def fetch_exam_info(url, exam_info, emne):
     # Parse HTML-innholdet
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Finn informasjon om eksamen
-   
-    
+       
     # Finn eksamensdato og tid
     exam_date_section = soup.find('p', class_='exam-date')
     if exam_date_section:
         
         exam_time_text = exam_date_section.text.strip("")
         exam_time_text = exam_time_text.strip("\n").split(",")
-        emne_in= "".join(exam_time_text).split()
+        topic_info= "".join(exam_time_text).split()
         
-        date = "".join(emne_in[1:3])
-        start_time = "".join(emne_in[4])
-        duration = " ".join(emne_in[5:]).strip(".")
+        date = "".join(topic_info[1:3])
+        start_time = "".join(topic_info[4])
+        duration = " ".join(topic_info[5:]).strip(".")
         duration = duration.lstrip("(")
         
-        exam_info[emne] = {"Date":date, "StartTime":start_time,"Duration":duration.rstrip(")")}
-        
+        exam_info[topic] = {"Date":date, "StartTime":start_time,"Duration":duration.rstrip(")")}
     
-    # Finn eksamenssted
-    """  exam_place_section = soup.find('p', class_='exam-place-info')
-        if exam_place_section:
-            places = [place.strip() for place in exam_place_section.stripped_strings]
-            exam_info['Sted'] = places
-        """
     return exam_info
 
 
@@ -75,11 +60,12 @@ def fetch_course_codes(url):
 
 
 def main():
+    
     bachelor = "https://www.uio.no/studier/emner/matnat/ifi/?filter.level=bachelor&filter.semester=v24"
     master = "https://www.uio.no/studier/emner/matnat/ifi/?filter.level=master&filter.semester=h24"
     
     courses = fetch_course_codes(bachelor)
-    emne_oversikt = defaultdict(str)
+    topic_overview = defaultdict(str)
     
     data = [
         ['Emnekode', 'Eksamendato', "Tidspunkt", "Varighet"]
@@ -87,19 +73,17 @@ def main():
     
     for emne in courses:
         url = f"https://www.uio.no/studier/emner/matnat/ifi/{emne}/v24/eksamen/index.html"
-        fetch_exam_info(url, emne_oversikt, emne)
+        fetch_exam_info(url, topic_overview, emne)
 
   
-    for key in emne_oversikt:
-        emne_info = [key, emne_oversikt[key]["Date"], emne_oversikt[key]["StartTime"], emne_oversikt[key]["Duration"]]
+    for key in topic_overview:
+        topic_info = [key, topic_overview[key]["Date"], topic_overview[key]["StartTime"], topic_overview[key]["Duration"]]
         
-        data.append(emne_info)
+        data.append(topic_info)
     
-    workbook = xlsxwriter.Workbook('data.xlsx')
+    workbook = xlsxwriter.Workbook('exam_calendar.xlsx')
     worksheet = workbook.add_worksheet()
     
-    
-
     for row_idx, row_data in enumerate(data):
         for col_idx, cell_data in enumerate(row_data):
             worksheet.write(row_idx, col_idx, cell_data)
